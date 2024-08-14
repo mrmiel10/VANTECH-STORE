20c735c7b8efd2328f31c1f92486f248b115fb86
Daryl <borismbakop611@gmail.com>
2024-08-14 22:53:30 +0100

    Update AddProductsForm.tsx

--- src/app/admin/dashboard/add-products/AddProductsForm.tsx
+++ src/app/admin/dashboard/add-products/AddProductsForm.tsx
@@ -15,7 +15,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import { Input } from "@/components/ui/input";
+
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 
