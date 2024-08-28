f5321e4993f52228394d50184e0b50577c7d2b70
Daryl <borismbakop611@gmail.com>
2024-08-28 04:10:15 +0100

    Update CardOrders.tsx

--- components/admin/orders/CardOrders.tsx
+++ components/admin/orders/CardOrders.tsx
@@ -17,73 +17,7 @@ import {
 } from "@/lib/actions";
 import { getStartAndEndOfPeriod } from "@/lib/getStartAndEndOfPeriod";
 
-export const CardAllOrders = async () => {
-  // await new Promise((resolve) => setTimeout(resolve, 10000));
-  const countAllOrders = await prisma.order.count();
-  const { _count: countPaidOrders, _sum: amountPaidOrders } =
-    await getAllPaidOrUnpaidOrders("paid");
-  const { _count: countUnpaidOrders, _sum: amountUnpaidOrders } =
-    await getAllPaidOrUnpaidOrders("pending");
-  return (
-    <Card>
-      <CardHeader className="pb-3">
-        <CardDescription className="max-w-lg text-balance ">
-          All
-        </CardDescription>
-        <CardTitle className="text-blue-500 text-3xl md:text-4xl">
-          {/* <Card className="h-16 w-52 flex justify-center items-center text-blue-500 antialiased border-2 border-blue-500 ">192 Orders</Card> */}
-          {countAllOrders} orders
-        </CardTitle>
-      </CardHeader>
-      <CardFooter>
-        <div className=" max-md:gap-4 gap-8 flex flex-row items-stretch text-muted-foreground text-sm">
-          <div className="flex-1 flex flex-col self-end  ">
-            {" "}
-            {!amountPaidOrders.amount ||
-              countPaidOrders === 0 ? (
-                <div>you have no paid orders yet</div>
-              ) : (
-                <div className="grid grid-cols-1 h-full">
-                  <div>
-                  
-                    {countPaidOrders} paid orders
-                  </div>
 
-                  <div className="flex self-end font-semibold text-blue-500 text-xs">
-                    {formatPrice(amountPaidOrders.amount!)}{" "}
-                
-                  </div>
-                </div>
-              )}
-          
-          </div>
-          <Separator orientation="vertical" className="h-full min-h-12" />
-          <div className="flex-1 flex flex-col  text-muted-foreground  border-muted  ">
-            {" "}
-            {!amountUnpaidOrders.amount ||
-              countUnpaidOrders === 0 ? (
-                <div>you have no unpaid orders</div>
-              ) : (
-                <div className="grid grid-cols-1 h-full">
-                  <div>
-                  
-                    {countUnpaidOrders} unpaid orders
-                  </div>
-
-                  <div className="flex self-end font-semibold text-blue-500 text-xs">
-                    {formatPrice(amountUnpaidOrders.amount!)}{" "}
-                
-                  </div>
-                </div>
-              )}
-          
-       
-          </div>
-        </div>
-      </CardFooter>
-    </Card>
-  );
-};
 export const CardWeekOrMonthOrders = async({name,description}:{
   name:"week" | "month" | "all",
   description:string
