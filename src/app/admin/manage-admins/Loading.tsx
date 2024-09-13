f0a8ca56db8c9b9ef75fa745dbb00f3aba777eb0
Daryl <borismbakop611@gmail.com>
2024-09-13 05:52:40 +0100

    Update Loading.tsx

--- src/app/admin/manage-admins/Loading.tsx
+++ src/app/admin/manage-admins/Loading.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import Loader from '../Loader'
-export const Loading = () => {
+import React from "react";
+import Loader from "../Loader";
+ const Loading = () => {
   return (
-    <div className='bg-white h-full w-full justify-center items-start'>
-
-<Loader />
+    <div className="bg-white h-full w-full justify-center items-start">
+      <Loader />
     </div>
-  )
-}
+  );
+};
+export default Loading
